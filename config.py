3297efcc6bc6748cfc31641ab85dbd410a92bf38
Luca <108634115+beatsbyluca@users.noreply.github.com>
2025-11-06 22:43:09 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,38 @@
+EMBED_CONFIG = {
+    "title": "",
+    "description": "",
+    "color": 0x5117ff,
+    "fields": [
+        {"name": "test", "value": "test", "inline": False},
+    ],
+    "image": "",
+    "footer": "",
+}
+
+SERVER_CONFIG = {
+    "new_name": "",
+    "new_icon": "",
+    "new_description": "",
+}
+
+WEBHOOK_CONFIG = {
+    "default_name": "",
+}
+
+
+AUTO_RAID_CONFIG = {
+    'num_channels': 100,
+    'channel_type': 'text',
+    'channel_name': '',
+    'num_messages': 500,
+    'message_content': ''
+}
+
+NO_BAN_KICK_ID = {
+    123,
+}
+
+BOT_PRESENCE = {
+    "type": "playing",
+    "text": "Balls"
+}
